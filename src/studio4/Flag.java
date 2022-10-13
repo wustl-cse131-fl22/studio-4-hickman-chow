e1152c5e49f28c6c6d2eba72cf4477be6ef05fdb
Dylan <Dylan@DESKTOP-5OLPTT0.dhcp.wustl.edu>
2022-10-13 12:41:41 -0500

    Haddad, Hickman-chow, Whalen studio done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,9 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		Color red = new Color(255, 0, 0);
+		StdDraw.setPenColor(red);
+		StdDraw.filledRectangle(1, 1, 1, 0.5);
+		
 	}
 }
